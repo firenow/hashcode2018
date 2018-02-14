7a9e35b6b1edbab9bafa3d3e7b3ff6088026a446
Thierry BARTHEL <thierry.barthel@bioserenity.com>
2018-02-14 14:26:34 +0100

    feat

--- src/io/IoSolution.java
+++ src/io/IoSolution.java
@@ -4,45 +4,38 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Collections;
-import java.util.Comparator;
-
-import model.Drone;
-import model.Instruction;
 import model.Solution;
 
 public class IoSolution {
 
-	public static void save(File repertory, Solution sol) {
-	/*	try {
-			File savedOutputFile = new File(repertory, getSolutionName(sol));
-			BufferedWriter bw = new BufferedWriter(new FileWriter(savedOutputFile));
+  public static void save(File repertory, Solution sol) {
+    try {
+      System.out.println("... Saving");
+      File savedOutputFile = new File(repertory, getSolutionName(sol));
+      BufferedWriter bw = new BufferedWriter(new FileWriter(savedOutputFile));
 
-			bw.write("" + sol.instructions.size());
-			bw.newLine();
-			
-			Collections.sort(sol.instructions, new Comparator<Instruction>() {
-				public int compare(Instruction i1, Instruction i2) {
-					return i1.turnNb - i2.turnNb;
-				}
-			});
-			
-			for (Instruction instr : sol.instructions) {
-				bw.write(instr.command);
-				bw.newLine();
-			}
+      bw.write(sol.slices.size());
+      bw.newLine();
 
-			bw.close();
-			System.out.println("... Saved : " + savedOutputFile.toString());
-		} catch (IOException e) {
-			System.out.println("... ERROR while saving file");
-			e.printStackTrace();
-		}*/
-	}
+      //
+      for (int i = 0; i < sol.slices.size(); i++) {
+        bw.write(sol.slices.get(i).toString());
 
-	private static String getSolutionName(Solution sol) {
-		//return sol.problem.name + "_" + sol.getScore() + ".txt";
-		return "";
-	}
+        //
+        if (i != sol.slices.size() - 1) {
+          bw.newLine();
+        }
+      }
 
+      bw.close();
+      System.out.println("... Saved : " + savedOutputFile.toString());
+    } catch (IOException e) {
+      System.out.println("... ERROR while saving file");
+      e.printStackTrace();
+    }
+  }
+
+  private static String getSolutionName(Solution sol) {
+    return sol.problem.getName() + "_" + sol.getScore() + ".txt";
+  }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,42 +1,45 @@
 package main;
 
-import io.IoProblem;
-import model.Problem;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
+import io.IoProblem;
+import io.IoSolution;
+import model.Problem;
+import model.Solution;
 
 public class Main {
-	
-	private static String[] fileNames = {"busy_day.in", "mother_of_all_warehouses.in", "redundancy.in"};
 
-	public static void main(String[] args)
-			throws InterruptedException, ExecutionException, TimeoutException, IOException {
-		for (String filename : fileNames) {
-			System.out.println("... [MAIN] - Launching");
-	
-			// Loading
-			System.out.println("... [MAIN] - Loading input data");
-			File problemFile = new File("src/resources", "medium.in");
+  String fileName = "medium.in";
 
-			Problem problem = IoProblem.load(problemFile);
-			
-			problem.print();
-			/*System.out.println("... [MAIN] - Ending");
-	
-			
-			// Basic solving
-			System.out.println("... [MAIN] - Solving problem");
-			Solution sol = new Solution(problem);
-			Greedy greedy = new Greedy(sol);
-			sol = greedy.solve();
-	
-			
-			IoSolution.save(new File("resources/out"), sol);*/
+  public static void main(String[] args)
+      throws InterruptedException, ExecutionException, TimeoutException, IOException {
+    // for (String filename : fileNames) {
+    System.out.println("... [MAIN] - Launching");
 
-		}
-	}
+    // Loading
+    System.out.println("... [MAIN] - Loading input data");
+    File problemFile = new File("src/resources", "medium.in");
+
+    Problem problem = IoProblem.load(problemFile);
+
+    problem.print();
+    Solution sol = new Solution(problem);
+    // sol = greedy.solve();
+    IoSolution.save(new File("src/resources/out"), sol);
+
+    /*
+     * System.out.println("... [MAIN] - Ending");
+     * 
+     * // Basic solving System.out.println("... [MAIN] - Solving problem"); Solution sol = new
+     * Solution(problem); Greedy greedy = new Greedy(sol); sol = greedy.solve();
+     * 
+     * 
+     * IoSolution.save(new File("resources/out"), sol);
+     */
+
+    // }
+  }
 
 }
