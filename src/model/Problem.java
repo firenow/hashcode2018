4218aa8663402936cb6e5ec9280056e43e271518
Maxime <maxime.leprince@bioserenity.com>
2018-02-14 13:39:13 +0100

    add problem model

--- src/model/Problem.java
+++ src/model/Problem.java
@@ -1,52 +1,34 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Problem {
 
-	public String name;
-	
-	public int rowsNb;
-	public int colsNb;
-	public int turnsNb;
-	public int maxPayload;
-	public int dronesNb;
-	
-	public List<Order> orders;
-	public List<Warehouse> warehouses;
-	
-	public List<Integer> orderWeights;
+	boolean[][] pizza;
 
-	
+	int rowNumber;
+	int columnNumber;
+	int minEachIngredientPerSlice;
+	int maxtotalIngredientsPerSlice;
 	public Problem(String name) {
-		// TODO Auto-generated constructor stub
-		this.orders = new ArrayList<Order> ();
-		this.warehouses = new ArrayList<Warehouse> ();
-		this.orderWeights = new ArrayList<Integer> ();
-		
-		this.name = name;
+
+	}
+
+	@Override
+	public String toString() {
+		return "Problem{" +
+				"pizza=" + Arrays.toString(pizza) +
+				", rowNumber=" + rowNumber +
+				", columnNumber=" + columnNumber +
+				", minEachIngredientPerSlice=" + minEachIngredientPerSlice +
+				", maxtotalIngredientsPerSlice=" + maxtotalIngredientsPerSlice +
+				'}';
 	}
 	
 	public void print() {
-		System.out.println("");
-		System.out.println("... Warehouses");
-		for (Warehouse wh : this.warehouses) {
-			System.out.print(wh.toString() + ", ");
-		}
-		
-		System.out.println("");
-		System.out.println("... Order Weights");
-		for (Integer orderWeight : this.orderWeights) {
-			System.out.print(orderWeight + ", ");
-		}
-		
-		System.out.println("");
-		System.out.println("... Orders");
-		for (Order order : this.orders) {
-			System.out.print(order.toString() + ", ");
-		}
-		
+		System.out.println(this.toString());
 	}
 	
 }
