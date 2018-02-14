4a8a8ad5c19a899cc8a5b68fc8f9725146715909
Maxime <maxime.leprince@bioserenity.com>
2018-02-14 13:58:01 +0100

    commit probleme

--- src/model/Problem.java
+++ src/model/Problem.java
@@ -3,17 +3,61 @@ package model;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 public class Problem {
 
 	boolean[][] pizza;
 
+	String name;
+
 	int rowNumber;
 	int columnNumber;
 	int minEachIngredientPerSlice;
 	int maxtotalIngredientsPerSlice;
-	public Problem(String name) {
 
+	public Problem(String name) {
+		this.name = name;
+	}
+
+	public boolean[][] getPizza() {
+		return pizza;
+	}
+
+	public void setPizza(boolean[][] pizza) {
+		this.pizza = pizza;
+	}
+
+	public int getRowNumber() {
+		return rowNumber;
+	}
+
+	public void setRowNumber(int rowNumber) {
+		this.rowNumber = rowNumber;
+	}
+
+	public int getColumnNumber() {
+		return columnNumber;
+	}
+
+	public void setColumnNumber(int columnNumber) {
+		this.columnNumber = columnNumber;
+	}
+
+	public int getMinEachIngredientPerSlice() {
+		return minEachIngredientPerSlice;
+	}
+
+	public void setMinEachIngredientPerSlice(int minEachIngredientPerSlice) {
+		this.minEachIngredientPerSlice = minEachIngredientPerSlice;
+	}
+
+	public int getMaxtotalIngredientsPerSlice() {
+		return maxtotalIngredientsPerSlice;
+	}
+
+	public void setMaxtotalIngredientsPerSlice(int maxtotalIngredientsPerSlice) {
+		this.maxtotalIngredientsPerSlice = maxtotalIngredientsPerSlice;
 	}
 
 	@Override
